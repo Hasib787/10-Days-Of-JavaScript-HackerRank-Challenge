35621e2e58b85447aebea4f26c934f38516c25fc
Md. Masud Rana <mr.hasib45@gmail.com>
2022-02-22 23:58:37 +0600

    solved

--- Day 1/Functions.js
+++ Day 1/Functions.js
@@ -2,9 +2,10 @@
  * Create the function factorial here
  */
 
-
-function main() {
-    const n = +(readLine());
-    
-    console.log(factorial(n));
+function factorial(n) {
+  if (n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
+
