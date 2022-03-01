c9ccd460d94368ba31cbc326dc0021f8e8a3d4ad
Md. Masud Rana <mr.hasib45@gmail.com>
2022-03-01 23:56:55 +0600

    added a file and practice hoisting

---
+++ practice.js
@@ -0,0 +1,14 @@
+const word = ["sky", "blue", "cloud", "sun", "moon", "rain", "wind", "tree"];
+// console.log(word.splice(2, 3));
+
+const add = (first, second) => {
+  const result = first + second;
+  console.log(mood);
+  if(result > 10) {
+      let mood = "happy";
+      console.log(mood);
+  }
+  return result;
+};
+const total = add(10, 20);
+// console.log(total);
