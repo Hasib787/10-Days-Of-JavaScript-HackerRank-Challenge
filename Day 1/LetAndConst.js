034be0b5d407bd3dfac4bc00e24a4bd14a57aae3
Md. Masud Rana <mr.hasib45@gmail.com>
2022-03-15 23:48:05 +0600

    let and const problem solved

--- Day 1/LetAndConst.js
+++ Day 1/LetAndConst.js
@@ -1,7 +1,11 @@
 function main() {
-    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
-    
-    // Print the area of the circle:
-    
-    // Print the perimeter of the circle:
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  const PI = Math.PI;
+  let r = parseFloat(readLine());
+  let area = PI * Math.pow(r, 2);
+  let perimeter = 2 * PI * r;
+  // Print the area of the circle:
+  console.log(area);
+  // Print the perimeter of the circle:
+  console.log(perimeter);
 }
--- practice.js
+++ practice.js
@@ -1,14 +1,44 @@
-const word = ["sky", "blue", "cloud", "sun", "moon", "rain", "wind", "tree"];
-// console.log(word.splice(2, 3));
+// const word = ["sky", "blue", "cloud", "sun", "moon", "rain", "wind", "tree"];
+// // console.log(word.splice(2, 3));
 
-const add = (first, second) => {
-  const result = first + second;
-  console.log(mood);
-  if(result > 10) {
-      let mood = "happy";
-      console.log(mood);
-  }
-  return result;
-};
-const total = add(10, 20);
-// console.log(total);
+// const add = (first, second) => {
+//   const result = first + second;
+//   if(result > 10) {
+//       let mood = "happy";
+//       // console.log(mood);
+//   }
+//   return result;
+// };
+// const total = add(10, 20);
+// // console.log(total);
+
+// const reverseStr = (str)=>{
+//     return str.split("").reverse().join("");
+// }
+// // console.log(reverseStr("I Love Code"));
+// function WordCount(str) {
+//   return str.split(" ").length;
+// }
+
+// console.log(WordCount("Random String"));
+
+// var a = 1, b=2;
+// [a,b]=[b,a]
+// console.log(a)
+// console.log(b)
+
+// const cart = {pen: 2}
+// cart.pencil = 1;
+// console.log(cart);
+
+// const sum = (a = 5, b = 10) => a + b;
+// console.log(sum(15,20));
+// console.log(sum(30));
+// console.log(sum(undefined,20));
+// console.log(sum());
+
+var arr1 = [1, 3];
+var arr2 =[arr1, [5,5]];
+var arr3 = [...arr2, [1, 8]];
+console.log(arr3);
+
